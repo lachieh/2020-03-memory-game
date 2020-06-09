28f265c4c825ed92a00b2006523db3cd8e80f826
Lachlan Heywood <lachlan@realtheory.io>
2020-06-09 11:09:46 -0400

    complete part two

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -4,10 +4,32 @@ import './MemoryCard.css';
 import logo from '../logo-wrench-white.png';
 
 class Card extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isFlipped: false,
+    };
+  }
+
+  clickHandler() {
+    this.setState({ isFlipped: !this.state.isFlipped });
+  }
+
   render() {
+    let innerClass = "MemoryCard__inner";
+    if (this.state.isFlipped) {
+      innerClass += ' flipped';
+    }
     return (
-      <div className="MemoryCard">
-        <img src={logo} alt="" />
+      <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+        <div className={innerClass}>
+          <div className="MemoryCard__back">
+            <img src={logo} alt="" />
+          </div>
+          <div className="MemoryCard__front">
+            ∆
+          </div>
+        </div>
       </div>
     )
   }
